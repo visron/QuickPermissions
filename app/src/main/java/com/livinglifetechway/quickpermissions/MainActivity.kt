f7d346a9250d6346115dc283f244128a6134047d
vis <hdronoh@gmail.com>
2021-10-07 15:02:01 +0300

    Android x and upgraded library versions

--- app/src/main/java/com/livinglifetechway/quickpermissions/MainActivity.kt
+++ app/src/main/java/com/livinglifetechway/quickpermissions/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.livinglifetechway.quickpermissions
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
